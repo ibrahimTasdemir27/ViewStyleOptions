55607399749ddac732269942379c415cb28460de
ibrahimtasdmr27 <ibrahimtasdmr27@gmail.com>
2024-10-18 23:44:45 +0300

    exported module

--- Package.swift
+++ Package.swift
@@ -16,10 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ViewStyleOptions",
-            swiftSettings: [
-                .enableExperimentalFeature("AccessLevelOnImport")
-            ]
+            name: "ViewStyleOptions"
         ),
         .testTarget(
             name: "ViewStyleOptionsTests",
---
+++ Sources/ViewStyleOptions/ViewStyleOptions/Exported.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by İbrahim Taşdemir on 18.10.2024.
+//
+
+import Foundation
+@_exported import ViewStyleOptions
