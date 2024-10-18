1766b54530eb4c771c70caa0a18325963e53afb7
ibrahimtasdmr27 <ibrahimtasdmr27@gmail.com>
2024-10-19 00:01:37 +0300

    public init - Border & Shadow

--- Sources/ViewStyleOptions/ViewStyleOptions/Model/ViewStyleOptions.swift
+++ Sources/ViewStyleOptions/ViewStyleOptions/Model/ViewStyleOptions.swift
@@ -16,6 +16,11 @@ public struct Border {
     let size: CGFloat
     let color: UIColor
     
+    public init(size: CGFloat, color: UIColor) {
+        self.size = size
+        self.color = color
+    }
+    
     public static let lightGray: Border = .init(size: 0.4, color: UIColor.darkGray)
 }
 
@@ -25,6 +30,13 @@ public struct Shadow {
     let opacity: Float
     let offset: CGSize
     let radius: CGFloat
+    
+    public init(color: UIColor, opacity: Float, offset: CGSize, radius: CGFloat) {
+        self.color = color
+        self.opacity = opacity
+        self.offset = offset
+        self.radius = radius
+    }
     
     public static let defaultShadow = Shadow(color: .black, opacity: 0.1, offset: CGSize(width: 0, height: 0.5), radius: 15)
 }
