130c6b0ec38a4962786ae5b0d0c8f6052ffd4157
ibrahimtasdmr27 <ibrahimtasdmr27@gmail.com>
2025-12-06 00:43:47 +0300

    1.1.0

--- Package.swift
+++ Package.swift
@@ -1,25 +1,32 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "ViewStyleOptions",
     platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ViewStyleOptions",
-            targets: ["ViewStyleOptions"]),
+            targets: ["ViewStyleOptions"]
+        ),
+    ],
+    dependencies: [
+        // SnapKit bağımlılığı
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ViewStyleOptions"
+            name: "ViewStyleOptions",
+            dependencies: [
+                "SnapKit"
+            ]
         ),
         .testTarget(
             name: "ViewStyleOptionsTests",
-            dependencies: ["ViewStyleOptions"]),
+            dependencies: ["ViewStyleOptions"]
+        ),
     ]
 )
